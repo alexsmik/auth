bda8cf8e8bbda3c48373a2f494a0b3fcc93f993f
alexsmik <alexsmik@yandex.ru>
2023-05-11 15:29:45 +0300

    upd

---
+++ client/src/pages/ForgotPassword.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import FormRow from '../components/FormRow';
+import axios from 'axios';
+import useLocalState from '../utils/localState';
+
+const ForgotPassword = () => {
+    const [email, setEmail] = useState('');
+    const {
+        alert,
+        showAlert,
+        loading,
+        setLoading,
+        success,
+        setSuccess,
+        hideAlert,
+    } = useLocalState();
+
+    const handleChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        hideAlert();
+        if (!email) {
+            showAlert({
+                text: 'Please provide email',
+            });
+            setLoading(false);
+            return;
+        }
+        try {
+            const { data } = await axios.post('/api/v1/auth/forgot-password', {
+                email,
+            });
+            showAlert({ text: data.msg, type: 'success' });
+            setSuccess(true);
+        } catch (error) {
+            showAlert({
+                text: 'Something went wrong, please try again',
+            });
+            setSuccess(true);
+        }
+        setLoading(false);
+    };
+    return (
+        <Wrapper className='page'>
+            {alert.show && (
+                <div className={`alert alert-${alert.type}`}>{alert.text}</div>
+            )}
+            {!success && (
+                <form
+                    className={loading ? 'form form-loading' : 'form'}
+                    onSubmit={handleSubmit}
+                >
+                    <h4>Forgot password</h4>
+                    {/* single form row */}
+                    <FormRow
+                        type='email'
+                        name='email'
+                        value={email}
+                        handleChange={handleChange}
+                    />
+                    {/* end of single form row */}
+                    <button type='submit' className='btn btn-block' disabled={loading}>
+                        {loading ? 'Please Wait...' : 'Get Reset Password Link'}
+                    </button>
+                    <p>
+                        Already a have an account?
+                        <Link to='/login' className='login-link'>
+                            Log In
+                        </Link>
+                    </p>
+                </form>
+            )}
+        </Wrapper>
+    );
+};
+
+export default ForgotPassword;
