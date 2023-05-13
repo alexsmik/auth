2ba4044bcc8d23ba8180d24db30e55811fd46cda
alexsmik <alexsmik@yandex.ru>
2023-05-13 22:00:36 +0300

    upd

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -150,7 +150,8 @@ const forgotPassword = async (req, res) => {
   if (user) {
     const passwordToken = crypto.randomBytes(70).toString('hex');
     // send email
-    const origin = 'http://localhost:3000';
+    const origin = 'https://auth-front-bwb6.onrender.com';
+    // const origin = 'http://localhost:3000';
     await sendResetPasswordEmail({
       name: user.name,
       email: user.email,
--- server/server.js
+++ server/server.js
@@ -18,6 +18,11 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
+//You can use this to check if your server is working
+app.get('/', (req, res) => {
+  res.send('Welcome to your server');
+});
+
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
@@ -32,10 +37,13 @@ app.use(errorHandlerMiddleware);
 const PORT = process.env.PORT || 5001;
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose
+      .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('DB CONNECTED'))
+      .catch((err) => console.log('DB CONNECTION ERR', err));
     app.listen(PORT, () => console.log(`Server started = ${PORT}`));
   } catch (error) {
     console.log(error);
