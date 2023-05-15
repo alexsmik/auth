d8e11307d9af8000fc8598f6729f1fbac07036a8
alexsmik <alexsmik@yandex.ru>
2023-05-15 14:47:32 +0300

    upd

---
+++ server/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const { authenticateUser } = require('../middleware/authentication');
+
+const {
+  register,
+  login,
+  logout
+} = require('../controllers/auth');
+
+router.post('/register', register);
+router.post('/login', login);
+router.delete('/logout', authenticateUser, logout);
+
+module.exports = router;
---
+++ server/utils/nodemailerConfig.js
@@ -0,0 +1,10 @@
+// https://ethereal.email/create
+
+module.exports = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'arden84@ethereal.email',
+        pass: 'fZjCgH87pKrXYqW16E'
+    }
+};
